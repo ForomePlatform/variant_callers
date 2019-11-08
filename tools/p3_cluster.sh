75b0e77ab07e0bd71cac8d21d579403d77d6f22a
mmcentre <michael.bouzinier@kvitneset.com>
2019-11-07 20:46:31 -0500

    MAB    Restructuring of BGM variant callers

--- tools/p3_cluster.sh
+++ tools/p3_cluster.sh
@@ -3,7 +3,7 @@
 ## source /net/lib/python3.3/bin/activate
 eval "$(/net/apps/x86_64/miniconda3/bin/conda shell.bash hook)"
 # conda activate callers
-export PYTHONPATH=`dirname $0`/../src/python
+export PYTHONPATH=/net/bgm/versions/master/forome_callers/src/python
 echo $PYTHONPATH
 python $*
 
